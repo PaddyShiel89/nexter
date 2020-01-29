d560b9d3239a230403252943fdbc56d3170edd4b
Patrick Shiel <PShiel@rusu-home.ad.rusu.org.uk>
2020-01-29 13:49:47 +0000

    Rebuilt gallery modal foundations

--- src/components/colGallery/colGallery.js
+++ src/components/colGallery/colGallery.js
@@ -24,50 +24,89 @@ const Gallery = () => {
     }
   `)
 
+  const images = data.allFile.nodes
+
+  const [modalOpen, toggleModal] = useState(false)
+  const [currentImg, setCurrentImg] = useState(0)
+
+  const handleToggleModal = (e, image) => {
+    e.preventDefault()
+
+    setCurrentImg(image)
+    
+    toggleModal(modalOpen ? false : true)
+  }
+
+  const handleModalImageNext = () => {
+    if(currentImg === images.length -1) {
+      setCurrentImg(0)
+    } else {
+      const newIndex = currentImg +1
+      setCurrentImg(newIndex)
+    }
+  }
+
+  const handleModalImagePrev = () => {
+    if(currentImg === 0) {
+      setCurrentImg(images.length -1)
+    } else {
+      const newIndex = currentImg -1
+      setCurrentImg(newIndex)
+    }
+  }
+
   return (
     <>
       <section className={styles.gallery}>
-        {data.allFile.nodes.map((img, index) => (
-          <GalleryItem img={img} index={index} key={img.childImageSharp.id} />
+        {images.map((img, index) => (
+          <Link
+            key={`${img.childImageSharp.id}Image`}
+            onClick={e => handleToggleModal(e, index)}
+            to={img.childImageSharp.fluid.src}
+          >
+            <Img
+              fluid={img.childImageSharp.fluid}
+              alt={`Home ${index}`}
+              className={styles.galleryImage}
+            />
+          </Link>
         ))}
       </section>
+      <Modal
+        modalOpen={modalOpen}
+        toggleModalFunction={() => toggleModal(modalOpen ? false : true)}
+      >
+        <div className={styles.modal}>
+          <Img
+            fluid={images[currentImg].childImageSharp.fluid}
+            alt={`Home ${currentImg}`}
+          />
+          <button onClick={() => handleModalImagePrev()}>Prev</button>
+          <button onClick={() => handleModalImageNext()}>Next</button>
+        </div>
+      </Modal>
     </>
   )
 }
 
 export default Gallery
 
-const GalleryItem = ({img, index}) => {
-  const [modalOpen, toggleModal] = useState(false)
-  const linkClick = e => {
-    e.preventDefault()
-    toggleModal(modalOpen ? false : true)
-  }
-  return (
-    <>
-      <Link
-        onClick={e => linkClick(e)}
-        to={img.childImageSharp.fluid.src}
-      >
-        <Img
-          fluid={img.childImageSharp.fluid}
-          alt={`Home ${index}`}
-          className={styles.galleryImage}
-        />
-      </Link>
+// const GalleryItem = ({ img, index }) => {
+//   return (
+//     <>
 
-      <Modal
-        modalOpen={modalOpen}
-        key={`${img.childImageSharp.id}Modal`}
-        toggleModalFunction={() => toggleModal(modalOpen ? false : true)}
-      >
-        <div className={styles.modal}>
-          <Img
-            fluid={img.childImageSharp.fluid}
-            alt={`Home ${index}`}
-          />
-        </div>
-      </Modal>
-    </>
-  )
-}
+//       <Modal
+//         modalOpen={modalOpen}
+//         key={`${img.childImageSharp.id}Modal`}
+//         toggleModalFunction={() => toggleModal(modalOpen ? false : true)}
+//       >
+//         <div className={styles.modal}>
+//           <Img
+//             fluid={img.childImageSharp.fluid}
+//             alt={`Home ${index}`}
+//           />
+//         </div>
+//       </Modal>
+//     </>
+//   )
+// }
